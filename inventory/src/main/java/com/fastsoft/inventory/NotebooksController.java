4cb2bdacf8e6b19433f18d5ca58b72dc4e95012c
pavel koval <pavel.koval@center2m.ru>
2024-10-10 17:44:47 +0300

    initial: инициализирован проект с traceId

---
+++ inventory/src/main/java/com/fastsoft/inventory/NotebooksController.java
@@ -0,0 +1,44 @@
+package com.fastsoft.inventory;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/notebooks")
+public class NotebooksController {
+
+
+  @GetMapping
+  public ResponseEntity<List<NotebookDto>> getProducts() {
+
+    log.warn("start");
+
+    var notebooks = List.of(
+        new NotebookDto("Macbook pro 16.2", 10),
+        new NotebookDto("Macbook air 13.6", 3)
+    );
+
+    notebooks.forEach(n -> {
+      log.info("book: {}", n);
+    });
+
+    log.warn("ready");
+
+    return ResponseEntity.ok(notebooks);
+  }
+
+  record NotebookDto(
+      String name,
+      Integer count
+  ) {
+
+  }
+
+}
---
+++ market/src/main/java/com/fastsoft/market/StudentsController.java
@@ -0,0 +1,58 @@
+package com.fastsoft.market;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping({"/products"})
+public class StudentsController {
+
+  private final WebClient webClient;
+  private final ObservationRegistry observationRegistry;
+
+  @GetMapping
+  public ResponseEntity<List<NotebookDto>> getProducts() {
+    Observation observation = Observation.start("serviceCall", observationRegistry);
+    log.warn("start");
+    try {
+      RestClient restClient = RestClient.create();
+      final Mono<NotebookDto[]> response = webClient.get().uri("/notebooks")
+          .accept(MediaType.APPLICATION_JSON)
+          .retrieve()
+          .bodyToMono(NotebookDto[].class);
+      final NotebookDto[] notebooks = response.block();
+      for (var n : notebooks) {
+        log.info("book: {}", n);
+      }
+
+      log.warn("end");
+      return ResponseEntity.ok(Arrays.asList(notebooks));
+    } finally {
+      observation.stop();
+    }
+
+
+  }
+
+  record NotebookDto(
+      String name,
+      Integer count
+  ) {
+
+  }
+
+}
